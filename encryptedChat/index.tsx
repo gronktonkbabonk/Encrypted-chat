a7b2d203bf9bebb0058859e5de57db1ecd9a109a
Leah Wolfschläger <e.wolfschlaeger@gmail.com>
2026-03-03 23:44:27 +0100

    Added logging

--- encryptedChat/index.tsx
+++ encryptedChat/index.tsx
@@ -8,6 +8,7 @@ import { addMessagePreEditListener, addMessagePreSendListener, removeMessagePreE
 import { updateMessage } from "@api/MessageUpdater";
 import definePlugin from "@utils/types";
 import { Message } from "@vencord/discord-types";
+import { log } from "console";
 
 const regexStartEnd = /START\|([a-zA-Z0-9+/]*?={0,3})\|END/;
 
@@ -103,6 +104,7 @@ async function messageEncrypt(inText: string): Promise<string> {
 async function tryMessageDecrypt(bytes: Uint8Array<ArrayBuffer>): Promise<undefined | string> {
     if ((bytes.length - IV_LEN - CHECKSUM_LEN) % AES_BLOCKSIZE !== 0) {
         // This can't be a valid payload since the sizes are wrong
+        log(`Message has valid Start End encoding, yet payload size (${bytes.length - IV_LEN - CHECKSUM_LEN}) is wrong (Should be a multiple of ${AES_BLOCKSIZE}).`);
         return;
     }
     const iv = bytes.slice(0, IV_LEN);
@@ -113,12 +115,15 @@ async function tryMessageDecrypt(bytes: Uint8Array<ArrayBuffer>): Promise<undefi
     const checksum = await createChecksum(decrypted);
 
     if (messageChecksum !== checksum) {
+        log("Message decryption was successful but checksums don't match. Your password is most likely wrong.");
         return;
     }
     let text;
     try {
         text = new TextDecoder().decode(decrypted);
     } catch {
+        log("Decrypted checksums match, yet encoding threw an exception. This is probably a malicious text injection or smt smt.");
+
         // Probably something fishy going on
         return;
     }
@@ -131,11 +136,18 @@ async function tryMessageDecrypt(bytes: Uint8Array<ArrayBuffer>): Promise<undefi
 function handleIncomingMessage(message: Message) {
     const matches = regexStartEnd.exec(message.content);
     if (!matches) {
+        log(`Incoming message (${message.content}) didn't match with the regex`);
         return;
     }
     const base64 = matches[0][0];
-
-    const bytes = Uint8Array.fromBase64(base64);
+    log(`Extracted base64 part: '${base64}'`);
+    let bytes;
+    try {
+        bytes = Uint8Array.fromBase64(base64);
+    } catch {
+        log("Extracted part wasn't valid base 64 (which should be impossible)");
+        return;
+    }
 
     tryMessageDecrypt(bytes).then(function (decrypted: string | undefined) {
         if (!decrypted) {
