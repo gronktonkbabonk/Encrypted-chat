9962876d5745d5de9393d769b8280d00e65c39f9
Leah Wolfschläger <e.wolfschlaeger@gmail.com>
2026-03-08 14:11:46 +0100

    Added message types and better Start end block matching functionality to dont allow bypass

--- index.tsx
+++ index.tsx
@@ -15,13 +15,16 @@ import { Message } from "@vencord/discord-types";
 import { encryptChatBarIcon, EncryptIcon } from "./encryptIcon";
 import { settings } from "./settings";
 
-const regexStartEnd = /START\|([a-zA-Z0-9+/]*?={0,3})\|END/;
+const regexStartEnd = /START\|([a-zA-Z0-9+/]*?={0,3})\|END/g;
 const regexPing = /<(@[0-9].{17})>/;
 
 const IV_LEN = 16;
-const TYPE_LEN = 1;
+const MESSGE_TYPE_LEN = 1;
 const CHECKSUM_LEN = 8; // Ought to be enuf
-const HEAD_LEN = IV_LEN + CHECKSUM_LEN;
+
+const HEAD_LEN = MESSGE_TYPE_LEN;
+
+const ENCRYPT_HEAD_LEN = IV_LEN + CHECKSUM_LEN;
 const AES_BLOCKSIZE = 16;
 const password = crypto.getRandomValues(new Uint8Array(32));
 let binary = "";
@@ -116,10 +119,11 @@ async function createChecksum(bytes: Uint8Array<ArrayBuffer>): Promise<Uint8Arra
 async function messageEncrypt(inText: string, channel_id: string): Promise<string> {
     const textBytes = new TextEncoder().encode(inText);
     const checksum = await createChecksum(textBytes);
-    // LOGGER.log(`Plain bytes: ${textBytes}`);
+    const header = new Uint8Array([MessageType.Encrypted]);
     // LOGGER.log(`Encrypt checksum: ${checksum}`);
     const { encrypted, iv } = await encrypt(inText, await getStandardKey(channel_id));
-    const messageBytes = concatArrayBuffers(iv, checksum, new Uint8Array(encrypted));
+    const messageBytes = concatArrayBuffers(header, iv, checksum, new Uint8Array(encrypted));
+    LOGGER.log(`Message bytes: ${messageBytes.byteLength}, encrypted bytes: ${encrypted.byteLength}`);
     return `START|${toBase64(messageBytes)}|END`;
 }
 
@@ -134,22 +138,50 @@ function uint8ArraysEqual(a, b) {
     return true;
 }
 
-async function tryMessageDecrypt(bytes: Uint8Array<ArrayBuffer>, channel_id: string): Promise<undefined | string> {
+
+async function tryMessageHandle(bytes: Uint8Array<ArrayBuffer>, channel_id: string): Promise<undefined | string> {
     const payloadLen = bytes.byteLength - HEAD_LEN;
     if (payloadLen < 0) {
         // This can't be a valid payload since the sizes are wrong
-        LOGGER.warn(`Message has valid Start End encoding, yet payload len (${payloadLen}) is wrong (Should be a multiple of ${AES_BLOCKSIZE} and bigger than 0).`);
+        LOGGER.warn(`Message has valid Start End encoding, yet payload len (${payloadLen}) is too small`);
         // return;
     }
-    const iv = bytes.slice(0, IV_LEN);
-    // LOGGER.log("Decrypt IVS: ", iv);
-    const messageChecksum = bytes.slice(IV_LEN, IV_LEN + CHECKSUM_LEN);
-    // LOGGER.log("Decrypt checksum: ", messageChecksum);
 
-    const encrypted = bytes.slice(IV_LEN + CHECKSUM_LEN, bytes.byteLength);
+    let read = 0;
+
+    const message_type = bytes.slice(read, MESSGE_TYPE_LEN);
+    read += MESSGE_TYPE_LEN;
+
+    switch (message_type[0]) {
+        case MessageType.Encrypted:
+            return tryMessageDecrypt(bytes.slice(read), channel_id);
+        default:
+            LOGGER.warn(`Unhandled message type: ${message_type[0]}. Maybe this is due to an outdated version`);
+
+    }
+
+}
+
+async function tryMessageDecrypt(bytes: Uint8Array<ArrayBuffer>, channel_id: string): Promise<undefined | string> {
+    const payloadLen = bytes.byteLength - ENCRYPT_HEAD_LEN;
+
+    if (payloadLen <= 0) {
+        LOGGER.warn(`Message has valid Start End encoding, yet payload len (${payloadLen}) is wrong Should be bigger than 0`);
+        return;
+    }
+
+    let read = 0;
+
+    const iv = bytes.slice(read, read + IV_LEN);
+    read += IV_LEN;
+
+    const messageChecksum = bytes.slice(read, read + CHECKSUM_LEN);
+    read += CHECKSUM_LEN;
+
+    const payload = bytes.slice(read, bytes.byteLength);
     // LOGGER.log("Encrypted Bytes: ", encrypted);
 
-    const decrypted = await decrypt(encrypted, await getStandardKey(channel_id), iv);
+    const decrypted = await decrypt(payload, await getStandardKey(channel_id), iv);
     // LOGGER.log("Decrypted ", decrypted);
 
     const checksum = await createChecksum(decrypted);
@@ -171,6 +203,8 @@ async function tryMessageDecrypt(bytes: Uint8Array<ArrayBuffer>, channel_id: str
     return text;
 }
 
+
+
 // Optimally we'd be doing this using discords delegate system, but eh
 
 function toBase64(bytes: Uint8Array) {
@@ -184,31 +218,37 @@ function toBase64(bytes: Uint8Array) {
 
 
 function handleIncomingMessage(message: Message) {
-    const matches = regexStartEnd.exec(message.content);
-    if (!matches) {
-        // LOGGER.info(`Incoming message (${message.content}) didn't match with the regex`);
-        return;
-    }
-    LOGGER.info(`Matches: '${matches}'`);
-    const base64 = matches[1];
-    // LOGGER.info(`Extracted base64 part: '${base64}'`);
-    let bytes;
-    try {
-        const binaryString = atob(base64);
-        bytes = Uint8Array.from(binaryString, c => c.charCodeAt(0));
-    } catch {
-        // LOGGER.error("Extracted part wasn't valid base 64 (which should be impossible)");
-        return;
-    }
-
-    tryMessageDecrypt(bytes, message.channel_id).then(function (decrypted: string | undefined) {
-        if (!decrypted) {
-            // This message probably wasn't encrypted to begin with
-            return;
+    while (true) {
+        const matches = regexStartEnd.exec(message.content);
+        if (!matches) {
+            // LOGGER.info(`Incoming message (${message.content}) didn't match with the regex`);
+            break;
         }
-        // This might be run even before the message has first rendered, is that bad? who knoes
-        updateMessage(message.channel_id, message.id, { content: decrypted });
-    });
+
+        const base64 = matches[1];
+        const { index } = matches;
+        const { length } = matches[0];
+        LOGGER.info(`Matches: '${matches}', ${index}, ${length}`);
+        // LOGGER.info(`Extracted base64 part: '${base64}'`);
+        let bytes;
+        try {
+            const binaryString = atob(base64);
+            bytes = Uint8Array.from(binaryString, c => c.charCodeAt(0));
+        } catch {
+            // LOGGER.error("Extracted part wasn't valid base 64 (which should be impossible)");
+            break;
+        }
+
+        tryMessageHandle(bytes, message.channel_id).then(function (decrypted: string | undefined) {
+            if (!decrypted) {
+                // This message probably wasn't encrypted to begin with
+                return;
+            }
+            // This shouldn't cause any raceconditions since ts runs on a single thread :skull:
+            // This might be run even before the message has first rendered, is that bad? who knoes
+            updateMessage(message.channel_id, message.id, { content: message.content.slice(0, index) + decrypted + message.content.slice(index + length) });
+        });
+    }
 
     return message.content;
 }
